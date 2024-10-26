37a1d40f91b6f845bd64b73880cf53d5ad5e6c7a
elton <Hernandezperezeltonyael@gmail.com>
2024-10-26 01:05:04 -0600

    Se realizo el programa Operadores_asignacion_compuestos.py

---
+++ Fundamentos-python/Operadores_en_Python/Operadores_asignacion_compuestos.py
@@ -0,0 +1,38 @@
+"""
+# Nombre: Elton Yael Hernández Pérez
+# Fecha: 18/10/2024
+# Descripción: Este programa demuestra el uso de operaciones aritméticas en Python con
+# el uso de operadores de asignación compuesta.
+"""
+
+# Solicita al usuario que ingrese dos números decimales y los convierte a tipo float
+num1 = float(input("Ingrese un número: "))  # Primer número ingresado
+num2 = float(input("Ingrese otro número: "))  # Segundo número ingresado
+
+# Suma 4 al primer número (num1) y muestra el resultado
+num1 += 4  # num1 = num1 + 4
+print(num1)  # Imprime el nuevo valor de num1
+
+# Resta 6 al segundo número (num2) y muestra el resultado
+num2 -= 6  # num2 = num2 - 6
+print(num2)  # Imprime el nuevo valor de num2
+
+# Multiplica el primer número (num1) por 21 y muestra el resultado
+num1 *= 21  # num1 = num1 * 21
+print(num1)  # Imprime el nuevo valor de num1
+
+# Resta 11 al segundo número (num2) y muestra el resultado
+num2 -= 11  # num2 = num2 - 11
+print(num2)  # Imprime el nuevo valor de num2
+
+# Calcula el módulo del primer número (num1) con 14 y muestra el resultado
+num1 %= 14  # num1 = num1 % 14
+print(num1)  # Imprime el nuevo valor de num1
+
+# Eleva el segundo número (num2) al cuadrado y muestra el resultado
+num2 **= 2  # num2 = num2 ** 2
+print(num2)  # Imprime el nuevo valor de num2
+
+# Realiza la división entera del segundo número (num2) por 3 y muestra el resultado
+num2 //= 3  # num2 = num2 // 3
+print(num2)  # Imprime el nuevo valor de num2
