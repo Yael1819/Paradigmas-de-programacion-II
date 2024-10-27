b2281df49ed4ac65793980d9173ecac69537d3c9
elton <Hernandezperezeltonyael@gmail.com>
2024-10-27 00:28:30 -0600

    Se realizo el programa Sentencia_if.py.py

---
+++ Sentencia_control/Sentencia_if.py
@@ -0,0 +1,14 @@
+# Nombre: Elton Yael Hernández Pérez
+# Fecha: 23/10/2024
+# Descripción:
+# Este programa verifica si el usuario es mayor de edad.
+# Pide la edad del usuario y evalúa si es mayor o igual a 18,
+# mostrando un mensaje acorde si cumple la condición.
+
+# Solicita al usuario que ingrese su edad
+edad = int(input("Ingrese su edad: "))
+
+# Verifica si la edad es mayor o igual a 18
+if edad >= 18:
+    print("Es mayor de edad")  # Muestra un mensaje si el usuario es mayor de edad
+
