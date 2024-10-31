f2be900ea10f0d6d596cb352515791ef66d9f799
elton <Hernandezperezeltonyael@gmail.com>
2024-10-31 09:06:18 -0600

    primer problema del examen

--- Examen-Parcial-1/Ejercicio1_lic_informatica.py
+++ Examen-Parcial-1/Ejercicio1_lic_informatica.py
@@ -1,18 +1,19 @@
 # Nombre: Elton Yael Hernández Pérez
 # Fecha: 30/10/2024
-print("***Licenciatura en informatica***")
-num = int(input("Ingrese  un número: "))
-i=1
-while i <= num:
-    print(i, end=" ,")
-    i = i + 1
-    if i%3 == 0 and i%5 == 0:
-        print("Licenciatura en Informática")
-        i = i + 1
-    if i%3 == 0 :
-        print("Licenciatura")
-        i = i + 1
+print("***Licenciatura en Informática***")
+num = int(input("Ingrese un número: "))
+i = 1
 
-    if i%5 == 0:
-        print("Informática")
-        i = i + 1
+while i <= num:
+
+    if i % 3 == 0 and i % 5 == 0:
+        print("Licenciatura en Informática", end="\n")
+    elif i % 3 == 0:
+        print("Licenciatura ",end=",")
+    elif i % 5 == 0:
+        print("Informática",end=",")
+    else:
+
+        print(i, end=", ")
+
+    i += 1
