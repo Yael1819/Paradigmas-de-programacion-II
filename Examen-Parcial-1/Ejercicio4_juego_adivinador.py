e6754e26086bc8e7c456419c69f1b1da87f50b7a
elton <Hernandezperezeltonyael@gmail.com>
2024-10-31 00:24:59 -0600

    Cuarto problema del examen

---
+++ Examen-Parcial-1/Ejercicio4_juego_adivinador.py
@@ -0,0 +1,23 @@
+# Nombre: Elton Yael Hernández Pérez
+# Fecha: 30/10/2024
+
+import random
+print("***Juego del adivinador**")
+i=1
+b=0
+num = int(input(f"Número de intentos: {i},Ingrese un número(1-100):"))
+while i <= 5:
+    adivina = random.randint(1, 100)
+    num = int(input(f"Número de intentos: {i},Ingrese un número(1-100):"))
+    if adivina > num:
+        print("El número a adivinar es mayor.")
+    elif adivina < num:
+        print("El número a adivinar es menor.")
+    elif adivina == num:
+        print("¡Felicidades! ¡Ganaste el juego!")
+        b=1
+        break
+    i = i + 1
+if b == 0:
+    print(f"Perdiste ,no pudiste adivinar el número :{adivina} ")
+print("Fin del juego")
