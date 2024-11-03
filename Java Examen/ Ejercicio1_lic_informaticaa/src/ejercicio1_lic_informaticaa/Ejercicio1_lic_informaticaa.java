2bb5a2672b6a9cc869b52407488cbceb644d0185
Elton Yael Hernández Pérez <163355046+Yael1819@users.noreply.github.com>
2024-11-03 05:32:02 -0600

    Add files via upload

---
+++ Java Examen/ Ejercicio1_lic_informaticaa/src/ejercicio1_lic_informaticaa/Ejercicio1_lic_informaticaa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1_lic_informaticaa;
+
+/**
+ *
+ * @author elton
+ */
+public class Ejercicio1_lic_informaticaa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
