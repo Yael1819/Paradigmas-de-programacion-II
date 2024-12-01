2e5b190548151cfa72256a1de03767ab8f49a5a9
elton <Hernandezperezeltonyael@gmail.com>
2024-12-01 12:36:55 -0600

    Ejercicio 5 de ciclos

--- Ciclos/Ciclos_ejercicio5.py
+++ Ciclos/Ciclos_ejercicio5.py
@@ -12,7 +12,6 @@ print(f"\n\t{GREEN}| PIRÁMIDE |{RESET}\n")
 
 n = int(input("Ingrese el número de filas: "))
 
-# Variables para construir el triángulo
 asterisco = " * "  # Representa un asterisco con espacio adicional para separación
 espacio = "   "     # Espacio entre caracteres, ajustable para formato
 
@@ -31,4 +30,4 @@ for i in range(1, n + 1):                 # Itera desde 1 hasta n
 print("\nTriángulo 3:")
 for i in range(n, 0, -1):                # Itera desde n hasta 1 (triángulo invertido)
     espacios = espacio * (n - i)         # Calcula los espacios necesarios para alinear a la derecha
-    print(f"{espacios}{asterisco * i}")   # Combina espacios y asteriscos para cada fila
+    print(f"{espacios}{asterisco * i}")  # Combina espacios y asteriscos para cada fila
