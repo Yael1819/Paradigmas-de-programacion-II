621a3c586b07cda740637258b3e511be6187db91
Elton Yael Hernández Pérez <163355046+Yael1819@users.noreply.github.com>
2025-01-24 22:11:18 -0600

    Add files via upload

---
+++ historial_piedra_papel_tijera/src/main/java/piedra_papel_tijera_historial/Ejercicio3_piedra_papel_tijera.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package piedra_papel_tijera_historial;
+
+/**
+ *
+ * @author elton
+ */
+public class Ejercicio3_piedra_papel_tijera {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
